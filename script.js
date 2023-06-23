66cf2475b3ba02906f674af8b20acff7b31bd839
jaimeserranodev <jaime.serrano.dev@gmail.com>
2023-06-23 08:32:26 +0100

    comenzando parte desktop

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const toggleMenuElement = document.getElementById('toggle-menu');
+    const mainMenuElement = document.getElementById('main-menu');
+  
+    toggleMenuElement.addEventListener('click', () => {
+      mainMenuElement.classList.toggle('main-menu--show');
+    });
+  });
